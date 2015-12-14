8430544a049ca989ea58ec5cc969775e0ec33442
Sandi Metz <sandi@sandimetz.com>
2015-12-14 08:30:17 -0500

    make medium solution more consistent

--- roman_numerals/roman_numerals_medium.rb
+++ roman_numerals/roman_numerals_medium.rb
@@ -4,7 +4,19 @@ module Roman
       RomanNumerals.new(self).to_s
     end
   end
+
+  refine String do
+    def to_subtractive_roman
+      RomanNumeralConversion.new(self).to_subtractive
+    end
+
+    def to_additive_roman
+      RomanNumeralConversion.new(self).to_additive
+    end
+  end
 end
+
+using Roman
 
 class RomanNumerals
   ROMAN_NUMERALS = {
@@ -16,19 +28,17 @@ class RomanNumerals
        5 => 'V',
        1 => 'I'  }
 
-  attr_reader :number, :additive, :subtractive
+  attr_reader :number
 
   def initialize(number)
-    @number       = number
-    @additive     = to_additive
-    @subtractive  = SubtractiveRomanNumerals.new(additive).to_s
+    @number = number
   end
 
   def to_s
-    subtractive
+    convert.to_subtractive_roman
   end
 
-  def to_additive
+  def convert
     result = ''
     ROMAN_NUMERALS.keys.reduce(number) {|to_be_converted, base_10_value|
       num_chars_needed, remainder = to_be_converted.divmod(base_10_value)
@@ -39,8 +49,9 @@ class RomanNumerals
   end
 end
 
-class SubtractiveRomanNumerals
-  SUBTRACTIVE_ROMAN = {
+class RomanNumeralConversion
+
+  LONG_TO_SHORT_MAP = {
     'DCCCC' => 'CM',  # 900
     'CCCC'  => 'CD',  # 400
     'LXXXX' => 'XC',  # 90
@@ -49,14 +60,21 @@ class SubtractiveRomanNumerals
     'IIII'  => 'IV' } # 4
 
   attr_reader :roman
-
   def initialize(roman)
     @roman = roman
   end
 
-  def to_s
-    SUBTRACTIVE_ROMAN.keys.reduce(roman) {|converted_roman, long_form|
-      converted_roman.gsub(/#{long_form}/, SUBTRACTIVE_ROMAN[long_form])
+  def to_additive
+    convert(LONG_TO_SHORT_MAP.invert)
+  end
+
+  def to_subtractive
+    convert(LONG_TO_SHORT_MAP)
+  end
+
+  def convert(map)
+    map.keys.reduce(roman) {|converted_roman, alternate_form|
+      converted_roman.gsub(/#{alternate_form}/, map[alternate_form])
     }
   end
 end
--- roman_numerals/roman_numerals_medium_test.rb
+++ roman_numerals/roman_numerals_medium_test.rb
@@ -2,6 +2,7 @@
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'roman_numerals_medium'
+
 using Roman
 
 class RomanNumeralsTest < Minitest::Test
@@ -78,80 +79,40 @@ class RomanNumeralsTest < Minitest::Test
   end
 end
 
-class AdditiveRomanNumeralsTest < Minitest::Test
-  def test_1
-    assert_equal 'I', RomanNumerals.new(1).to_additive
-  end
 
-  def test_2
-    assert_equal 'II', RomanNumerals.new(2).to_additive
-  end
-
-  def test_3
-    assert_equal 'III', RomanNumerals.new(3).to_additive
-  end
+class RomanNumeralConversionTest < Minitest::Test
+    def test_2
+      assert_equal 'II', RomanNumeralConversion.new('II').to_subtractive
+      assert_equal 'II', RomanNumeralConversion.new('II').to_additive
+    end
 
   def test_4
-    assert_equal 'IIII', RomanNumerals.new(4).to_additive
-  end
-
-  def test_5
-    assert_equal 'V', RomanNumerals.new(5).to_additive
-  end
-
-  def test_6
-    assert_equal 'VI', RomanNumerals.new(6).to_additive
+    assert_equal 'IV',   RomanNumeralConversion.new('IIII').to_subtractive
+    assert_equal 'IIII', RomanNumeralConversion.new('IV').to_additive
   end
 
   def test_9
-    assert_equal 'VIIII', RomanNumerals.new(9).to_additive
-  end
-
-  def test_48
-    assert_equal 'XXXXVIII', RomanNumerals.new(48).to_additive
-  end
-
-  def test_99
-    assert_equal 'LXXXXVIIII', RomanNumerals.new(99).to_additive
-  end
-
-  def test_299
-    assert_equal 'CCLXXXXVIIII', RomanNumerals.new(299).to_additive
-  end
-
-  def test_300
-    assert_equal 'CCC', RomanNumerals.new(300).to_additive
-  end
-
-  def test_3000
-    assert_equal 'MMM', RomanNumerals.new(3000).to_additive
-  end
-
-end
-
-
-class SubtractiveRomanNumeralsTest < Minitest::Test
-  def test_4
-    assert_equal 'IV', SubtractiveRomanNumerals.new('IIII').to_s
-  end
-
-  def test_9
-    assert_equal 'IX', SubtractiveRomanNumerals.new('VIIII').to_s
+    assert_equal 'IX',    RomanNumeralConversion.new('VIIII').to_subtractive
+    assert_equal 'VIIII', RomanNumeralConversion.new('IX').to_additive
   end
 
   def test_49
-    assert_equal 'XLIX', SubtractiveRomanNumerals.new('XXXXVIIII').to_s
+    assert_equal 'XLIX',      RomanNumeralConversion.new('XXXXVIIII').to_subtractive
+    assert_equal 'XXXXVIIII', RomanNumeralConversion.new('XLIX').to_additive
   end
 
   def test_99
-    assert_equal 'XCIX', SubtractiveRomanNumerals.new('LXXXXVIIII').to_s
+    assert_equal 'XCIX',        RomanNumeralConversion.new('LXXXXVIIII').to_subtractive
+    assert_equal 'LXXXXVIIII',  RomanNumeralConversion.new('XCIX').to_additive
   end
 
   def test_449
-    assert_equal 'CDXLIX', SubtractiveRomanNumerals.new('CCCCXXXXVIIII').to_s
+    assert_equal 'CDXLIX',        RomanNumeralConversion.new('CCCCXXXXVIIII').to_subtractive
+    assert_equal 'CCCCXXXXVIIII', RomanNumeralConversion.new('CDXLIX').to_additive
   end
 
   def test_999
-    assert_equal 'XCIX', SubtractiveRomanNumerals.new('LXXXXVIIII').to_s
+    assert_equal 'XCIX',        RomanNumeralConversion.new('LXXXXVIIII').to_subtractive
+    assert_equal 'LXXXXVIIII',  RomanNumeralConversion.new('XCIX').to_additive
   end
 end
